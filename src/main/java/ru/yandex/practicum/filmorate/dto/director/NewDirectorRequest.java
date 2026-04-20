5989105a4c05fd47881edf02b1bc8e565de722a9
Lev <leviksun.s@gmail.com>
2026-04-20 08:55:23 +0300

    поправил checkStyle

--- src/main/java/ru/yandex/practicum/filmorate/dto/director/NewDirectorRequest.java
+++ src/main/java/ru/yandex/practicum/filmorate/dto/director/NewDirectorRequest.java
@@ -2,9 +2,6 @@ package ru.yandex.practicum.filmorate.dto.director;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
-import ru.yandex.practicum.filmorate.model.Film;
-
-import java.util.Set;
 
 @Data
 public class NewDirectorRequest {
--- src/main/java/ru/yandex/practicum/filmorate/model/Director.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Director.java
@@ -2,14 +2,13 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.Data;
 
-import java.util.Set;
 
 @Data
 public class Director {
     private  long id;
     private String name;
 
-    public static Director fromId(long id){
+    public static Director fromId(long id) {
         Director director = new Director();
         director.setId(id);
         return director;
