cb731be834d43447c34cb2f8c120a436d891c1c2
Lev <leviksun.s@gmail.com>
2026-04-20 09:02:54 +0300

    поправил sql director

--- src/main/java/ru/yandex/practicum/filmorate/dto/director/UpdateDirectorRequest.java
+++ src/main/java/ru/yandex/practicum/filmorate/dto/director/UpdateDirectorRequest.java
@@ -2,17 +2,13 @@ package ru.yandex.practicum.filmorate.dto.director;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
-import ru.yandex.practicum.filmorate.model.Film;
 
-import java.util.Set;
 
 @Data
 public class UpdateDirectorRequest {
     private final Long id;
     @NotBlank(message = "имя не должно быть пустым")
     private String name;
-    private Set<Film> films;
-
     public  boolean hasName() {
         return !(name == null || name.isBlank());
     }
--- src/main/java/ru/yandex/practicum/filmorate/storage/dal/sql/DirectorSql.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/dal/sql/DirectorSql.java
@@ -6,5 +6,5 @@ public class DirectorSql {
     public static final String CREATE = "sql/directors/create_director.sql";
     public static final String UPDATE = "sql/directors/update_director.sql";
     public static final String DELETE_BY_ID = "sql/directors/delete_director.sql";
-    public static final String FIND_FILMS_BY_DIRECTOR_IDS = "sql/directors_vis_films/find_films_by_director_ids/sql";
+    public static final String FIND_FILMS_BY_DIRECTOR_IDS = "sql/directors_vis_films/find_films_by_director_ids.sql";
 }
