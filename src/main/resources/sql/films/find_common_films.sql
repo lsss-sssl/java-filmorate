3601c0a4e2c25e5d201b57dd4edfed3e226fb99c
Lev <leviksun.s@gmail.com>
2026-04-23 01:35:18 +0300

    поправил sql

--- src/main/resources/sql/films/find_common_films.sql
+++ src/main/resources/sql/films/find_common_films.sql
@@ -1,8 +1,8 @@
-SELECT f*
+SELECT f.*
 FROM films f
-JOIN film_likes fl1 ON f.id = fl1.film_id;
+JOIN film_likes fl1 ON f.id = fl1.film_id
 JOIN film_likes fl2 ON f.id = fl2.film_id
 WHERE fl1.user_id = ?
    AND fl2.user_id = ?
 GROUP BY f.id, f.name, f.description, f.release_date, f.duration, f.mpa_id
-ORDER BY COUNT(fl.user_id) DESC, f.id;
+ORDER BY COUNT(*) DESC, f.id;
